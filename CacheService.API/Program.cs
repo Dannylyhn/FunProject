16659d6899fe022e1ff516b563aee030ba81a7c3
Danny Ly <dannylyhn@gmail.com>
2024-04-14 15:28:03 +0200

    WIP

--- CacheService.API/Program.cs
+++ CacheService.API/Program.cs
@@ -4,12 +4,21 @@ using CacheService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var test = config.GetValue<string>("Redis:Url");
+
+Console.WriteLine(test);
 
 builder.Services.AddSingleton<IRedisClientsManager>(new RedisManagerPool("localhost:6379"));
 builder.Services.AddScoped<ICacheService, CacheService.Services.CacheService>();
